097e389734750d9244613f74e9b0fd43df693473
Andrey Zhygarev <infernalsteel666@gmail.com>
2021-09-30 22:36:59 +0300

    016 Object Types

--- 02 Understanding-ts/app.ts
+++ 02 Understanding-ts/app.ts
@@ -1,17 +1,10 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  const result = n1 + n2;
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+const person = {
+  name: "Adrey",
+  age: 30,
+};
 
-  if (showResult) {
-    console.log(phrase + result);
-  } else {
-    return n1 + n2;
-  }
-}
-
-let number1: number;
-number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-let resulPhrase = "Result is: ";
-
-add(number1, number2, printResult, resulPhrase);
+console.log(person.name);
