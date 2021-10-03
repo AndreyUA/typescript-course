6a19e62a1fcfdc7619c7e8a9b717cbe742316547
Andrey Zhygarev <infernalsteel666@gmail.com>
2021-10-03 11:39:41 +0300

    024 Type Aliases _ Custom Types

--- 02 Understanding-ts/app.js
+++ 02 Understanding-ts/app.js
@@ -1,4 +1,3 @@
-// Union type
 function combine(input1, input2, resultConversion) {
     var result;
     if ((typeof input1 === "number" && typeof input2 === "number") ||
--- 02 Understanding-ts/app.ts
+++ 02 Understanding-ts/app.ts
@@ -1,12 +1,14 @@
-// Union type
+type Combinable = number | string;
+type ConversionDescriptor = "as-number" | "as-text";
+
 function combine(
-  input1: number | string,
-  input2: number | string,
-  resultConversion: "as-number" | "as-text"
+  input1: Combinable,
+  input2: Combinable,
+  resultConversion: ConversionDescriptor
 ) {
   let result;
 
-  if (
+  if ( 
     (typeof input1 === "number" && typeof input2 === "number") ||
     resultConversion === "as-number"
   ) {
