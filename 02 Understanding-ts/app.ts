1cd3143bdcada7cda0204e830a6a35a0e2e4b249
Andrey Zhygarev <infernalsteel666@gmail.com>
2021-10-03 11:34:20 +0300

    023 Literal Types

--- 02 Understanding-ts/app.ts
+++ 02 Understanding-ts/app.ts
@@ -1,18 +1,34 @@
 // Union type
-function combine(input1: number | string, input2: number | string) {
+function combine(
+  input1: number | string,
+  input2: number | string,
+  resultConversion: "as-number" | "as-text"
+) {
   let result;
 
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    result = input1 + input2;
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversion === "as-number"
+  ) {
+    result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
   }
 
+  // if (resultConversion === "as-number") {
+  //   return +result;
+  // } else {
+  //   return  result.toString();
+  // }
+
   return result;
 }
 
-const combineAges = combine(30, 26);
+const combineAges = combine(30, 26, "as-number");
 console.log(combineAges);
 
-const combinedNames = combine("Andrey", "Olha");
+const combineStringAges = combine("30", "26", "as-number");
+console.log(combineStringAges);
+
+const combinedNames = combine("Andrey", "Olha", "as-text");
 console.log(combinedNames);
