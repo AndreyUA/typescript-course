546f379ad791bd5500182f6eb01bdf327ebbabc2
Andrey Zhygarev <infernalsteel666@gmail.com>
2021-10-12 21:23:35 +0300

    030 The _never_ Type

--- 02 Understanding-ts/app.js
+++ 02 Understanding-ts/app.js
@@ -1,3 +1,17 @@
+// Unknown
 var userInput;
+var userName;
 userInput = 6;
 userInput = "Andrey";
+if (typeof userInput === "string") {
+    userName = userInput;
+}
+// Never
+function generateError(message, code) {
+    throw {
+        message: message,
+        errorCode: code
+    };
+}
+var result = generateError("An error occurred!", 500);
+console.log(result);
--- 02 Understanding-ts/app.ts
+++ 02 Understanding-ts/app.ts
@@ -1,3 +1,4 @@
+// Unknown
 let userInput: unknown;
 let userName: string;
 
@@ -7,3 +8,19 @@ userInput = "Andrey";
 if (typeof userInput === "string") {
   userName = userInput;
 }
+
+// Never
+function generateError(message: string, code: number): never {
+  throw {
+    message,
+    errorCode: code,
+  };
+
+  // while (true) {
+    
+  // }
+}
+
+// const result = generateError("An error occurred!", 500);
+
+// console.log(result);
