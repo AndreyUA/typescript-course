689f8b7e4b570fb77a8d22196e035e1575b2a238
Andrey Zhygarev <infernalsteel666@gmail.com>
2021-10-17 21:07:29 +0300

    049 _let_ and _const_

--- 04 Next-generation JavaScript & TypeScript/src/app.ts
+++ 04 Next-generation JavaScript & TypeScript/src/app.ts
@@ -1,1 +1,19 @@
-// Code goes here!
+const userName = "Andrey";
+
+let age = 30;
+age = 31;
+
+function add(a: number, b: number) {
+  let result;
+
+  result = a + b;
+
+  return result;
+}
+
+// if (age > 20) {
+//   var isOld = true;
+// }
+
+// Will work in valilla JS
+// console.log(isOld);
