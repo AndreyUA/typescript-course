52328a82b1272102221253ae90af747e204c3e84
Andrey Zhygarev <infernalsteel666@gmail.com>
2021-10-17 15:24:24 +0300

    044 Code Quality Options

--- 03 The TypeScript Compiler/src/app.ts
+++ 03 The TypeScript Compiler/src/app.ts
@@ -1,11 +1,23 @@
+let appId = "test";
+
 const button = document.querySelector("button");
+
+const add = (n1: number, n2: number) => {
+  if (n1 + n2 > 0) {
+    return n1 + n2;
+  }
+
+  return;
+};
 
 // Test comment
 
 const clickHandler = (message: string) => {
+  // let userName = "Andrey";
+
   console.log("clicked!!! " + message);
 };
 
 if (button) {
-  button.addEventListener("click", clickHandler.bind(null, 'Hello!'));
+  button.addEventListener("click", clickHandler.bind(null, "Hello!"));
 }
