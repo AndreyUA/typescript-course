3b7dbf8ba50b336fb4759855b68b59bed12ef328
Andrey Zhygarev <infernalsteel666@gmail.com>
2022-02-20 15:11:55 +0200

    085 Discriminated Unions

--- 06 Advanced Types/src/app.ts
+++ 06 Advanced Types/src/app.ts
@@ -93,3 +93,36 @@ useVehicle(v1);
 console.log("--------------------------");
 useVehicle(v2);
 console.log("--------------------------");
+
+// Discriminated unions
+
+interface Bird {
+  type: "bird";
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: "horse";
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+const moveAnimal = (animal: Animal) => {
+  let speed;
+
+  switch (animal.type) {
+    case "bird":
+      speed = animal.flyingSpeed;
+      break;
+    case "horse":
+      speed = animal.runningSpeed;
+      break;
+  }
+
+  console.log(`Moving at speed: ${speed}`);
+};
+
+moveAnimal({ type: "bird", flyingSpeed: 60 });
+console.log("--------------------------");
+moveAnimal({ type: "horse", runningSpeed: 65 });
