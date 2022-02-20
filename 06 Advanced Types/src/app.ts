fc32273dcb59061eb63d24e1348daa60134cf54b
Andrey Zhygarev <infernalsteel666@gmail.com>
2022-02-20 14:58:43 +0200

    084 More on Type Guards

--- 06 Advanced Types/src/app.ts
+++ 06 Advanced Types/src/app.ts
@@ -13,7 +13,7 @@ type Employee = {
 
 type ElevatedEmployee = Admin & Employee;
 
-const el: ElevatedEmployee = {
+const e1: ElevatedEmployee = {
   name: "Andrey",
   privileges: ["create-server"],
   startDate: new Date(),
@@ -22,3 +22,74 @@ const el: ElevatedEmployee = {
 type Combinable = string | number;
 type Numeric = number | boolean;
 type Universal = Combinable & Numeric;
+
+// Type guards
+
+const add = (a: Combinable, b: Combinable): string | number => {
+  if ("string" === typeof a || "string" === typeof b) {
+    return a.toString() + b.toString();
+  }
+
+  return a + b;
+};
+
+type UnknownEmployee = Employee | Admin;
+
+const printEmployeeInformation = (emp: UnknownEmployee) => {
+  console.log(`Name: ${emp.name}`);
+
+  if ("privileges" in emp) {
+    console.log(`Privileges: ${emp.privileges}`);
+  }
+
+  if ("startDate" in emp) {
+    console.log(`Start date: ${emp.startDate}`);
+  }
+};
+
+printEmployeeInformation(e1);
+console.log("--------------------------");
+printEmployeeInformation({
+  name: "Admin",
+  privileges: ["shut down the server"],
+});
+console.log("--------------------------");
+printEmployeeInformation({
+  name: "Employee",
+  startDate: new Date(),
+});
+console.log("--------------------------");
+
+class Car {
+  drive() {
+    console.log("Driving...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Driving a truck...");
+  }
+
+  loadCargo(amount: number) {
+    console.log(`Loading cargo... ${amount}`);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+const useVehicle = (vehicle: Vehicle) => {
+  vehicle.drive();
+
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+};
+
+useVehicle(v1);
+console.log("--------------------------");
+useVehicle(v2);
+console.log("--------------------------");
